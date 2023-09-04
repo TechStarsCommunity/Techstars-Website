932c64b383d06868679249d4f700176a9278940e
canhamzacode <abdulmuizzayo6@gmail.com>
2023-09-04 12:44:34 +0200

    setup the blog page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './Layout.jsx/AppLayout'
 import Login from './page/Login'
 import SignUp from './page/SignUp'
+import Blogs from './page/Blogs'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<LandingPage />} />
+          <Route path="/blogs" element={<Blogs />} />
         </Route>
         <Route path="/" element={""}>
           <Route path="/login" element={<Login />} />
