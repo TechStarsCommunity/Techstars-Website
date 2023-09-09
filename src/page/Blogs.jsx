e9a2cae2117d453bdeee9dfca4e05227629e6b00
canhamzacode <abdulmuizzayo6@gmail.com>
2023-09-09 20:09:34 +0200

    made change

--- src/page/Blogs.jsx
+++ src/page/Blogs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import BlogCard from '../components/BlogCard'
+import ScrollReveal from '../provider/ScrollReveal'
 
 const Blogs = () => {
     return (
@@ -10,30 +11,41 @@ const Blogs = () => {
             exit={{ opacity: 0, transition: { duration: 0.4 } }}
         >
             <div className='w-full flex justify-between items-center py-[30px] '>
-                <button className='rounded-tl-xl rounded-br-xl rounded-tr-xl border border-black p-[10px] font-bold'>
-                    <h1 className='text-1xl md:text-2xl'>
-                        Our Blog
-                    </h1>
-                </button>
-                <div className='flex gap-[25px] font-semibold '>
-                    <button className='rounded-xl border border-[#808080] px-[30px] py-[10px] font-bold bg-[#808080] text-white'>
-                        <h1 className='text-base'>
-                            All
+                <ScrollReveal direction="left">
+                    <button className='rounded-tl-xl rounded-br-xl rounded-tr-xl border border-black p-[10px] font-bold'>
+                        <h1 className='text-1xl md:text-2xl'>
+                            Our Blog
                         </h1>
                     </button>
-                    <button className='rounded-xl border border-[#808080] px-[30px] py-[10px] font-bold text-black'>
-                        <h1 className='text-base'>
-                            New
-                        </h1>
-                    </button>
-                </div>
+                </ScrollReveal>
+                <ScrollReveal direction="right">
+                    <div className='flex gap-[25px] font-semibold '>
+                        <button className='rounded-xl border border-[#808080] px-[30px] py-[10px] font-bold bg-[#808080] text-white'>
+                            <h1 className='text-base'>
+                                All
+                            </h1>
+                        </button>
+                        <button className='rounded-xl border border-[#808080] px-[30px] py-[10px] font-bold text-black'>
+                            <h1 className='text-base'>
+                                New
+                            </h1>
+                        </button>
+                    </div>
+                </ScrollReveal>
             </div>
             <div className='w-full grid gap-[30px] mb-[20px]'>
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
+                <ScrollReveal direction="left">
+                    <BlogCard />
+                </ScrollReveal>
+                <ScrollReveal direction="right">
+                    <BlogCard />
+                </ScrollReveal>
+                <ScrollReveal direction="left">
+                    <BlogCard />
+                </ScrollReveal>
+                <ScrollReveal direction="right">
+                    <BlogCard />
+                </ScrollReveal>
             </div>
         </motion.div>
     )
